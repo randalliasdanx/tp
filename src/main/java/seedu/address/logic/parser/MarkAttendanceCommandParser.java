a483bd787072e68131e7dc571aed8fadd0990c81
ray <raychua0106@gmail.com>
2026-04-07 15:41:57 +0800

    Create MarkAttendanceCommandParser.java and register in AddressBookParser

---
+++ src/main/java/seedu/address/logic/parser/MarkAttendanceCommandParser.java
@@ -0,0 +1,59 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE_STATUS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LESSON;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
+
+import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.MarkAttendanceCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.AttendanceStatus;
+
+/**
+ * Parses input arguments and creates a new MarkAttendanceCommand object.
+ */
+public class MarkAttendanceCommandParser implements Parser<MarkAttendanceCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the MarkAttendanceCommand
+     * and returns a MarkAttendanceCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public MarkAttendanceCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(
+                args, PREFIX_SUBJECT, PREFIX_LESSON, PREFIX_ATTENDANCE_STATUS);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_SUBJECT, PREFIX_LESSON, PREFIX_ATTENDANCE_STATUS)
+                || argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkAttendanceCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_SUBJECT, PREFIX_LESSON, PREFIX_ATTENDANCE_STATUS);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    "Invalid index: " + pe.getMessage() + "\n" + MarkAttendanceCommand.MESSAGE_USAGE, pe);
+        }
+
+        String subject = ParserUtil.parseSubject(argMultimap.getValue(PREFIX_SUBJECT).get()).subjectName;
+        String lesson = ParserUtil.parseLesson(argMultimap.getValue(PREFIX_LESSON).get());
+        AttendanceStatus status = ParserUtil.parseAttendanceStatus(
+                argMultimap.getValue(PREFIX_ATTENDANCE_STATUS).get());
+
+        return new MarkAttendanceCommand(index, subject, lesson, status);
+    }
+
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
